c472bfce06f1d87fc4c0078b9bde5043bae4e346
r_i_c_k <r_i_c_k@RICK>
2017-04-22 21:29:05 +0800

    4/22-21:30

---
+++ RO11.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RO11 : MonoBehaviour {
+    public GameObject Target;
+    public float MoveSpeed;
+    public float FindRange;
+    public bool Lock;
+    public bool Patrol=false;
+    private Rigidbody rigidbody;
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Patrol)
+        {
+            rigidbody.velocity = new Vector3(0, 0, MoveSpeed);
+        }
+        else
+        {
+            rigidbody.velocity = new Vector3(0, 0, -MoveSpeed);
+        }
+	}
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "wall")
+        {
+            Patrol = !Patrol;
+        }
+    }
+}
---
+++ monster_AI.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monster_AI : MonoBehaviour {
+    public Sprite monster;
+
+
+
+}
